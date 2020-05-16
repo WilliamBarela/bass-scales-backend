d062cbd6ca53a47eb9a49678d460f875eb9f10eb
William Barela <william.barela.dev@gmail.com>
2020-05-16 15:28:37 -0500

    add Feeling.count_by_scale_id scope method to collect counts of all adjectives for a given scale

--- app/models/feeling.rb
+++ app/models/feeling.rb
@@ -2,17 +2,8 @@ class Feeling < ApplicationRecord
   belongs_to :scale
 
   # FIXME: https://www.w3resource.com/sql/aggregate-functions/count-with-group-by.php
-  scope :count_by_scale_id, ->(scale_id) { find_by_sql ["SELECT adjective, COUNT(*) AS fcount, :scale_id as SCALE_ID FROM feelings WHERE scale_id=:scale_id GROUP BY adjective ORDER BY fcount DESC;", {:scale_id => scale_id}] }
-
+  scope :count_by_scale_id, ->(scale_id) { find_by_sql ["SELECT adjective, COUNT(*) AS count, :scale_id as SCALE_ID FROM feelings WHERE scale_id=:scale_id GROUP BY adjective ORDER BY count DESC;", {:scale_id => scale_id}] }
+ 
   # READ: https://www.postgresql.org/docs/9.6/functions-matching.html#FUNCTIONS-POSIX-TABLE
   # scope :find_author, ->(search_params) { where("first_name ~* ?", "^#{search_params[:query]}")}
-
-
-  # replaced by scope method :most_stories
-  # def self.most_stories
-  #   Author
-  #     .left_joins(:stories)
-  #     .group("authors.id")
-  #     .order("COUNT(stories.author_id) DESC")
-  # end
 end
