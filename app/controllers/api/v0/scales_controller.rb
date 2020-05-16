6c45c4e2d8c0ae83a5a468b34bcd63fab7b132d4
William Barela <william.barela.dev@gmail.com>
2020-05-16 04:48:52 -0500

    remove feelings...they were unnecessary

--- app/controllers/api/v0/scales_controller.rb
+++ app/controllers/api/v0/scales_controller.rb
@@ -1,7 +1,6 @@
 class Api::V0::ScalesController < ApplicationController
   def show
     scale = Scale.find_by(id: params[:id])
-    feelings = scale.feelings
     render json: ScaleSerializer.new(scale).to_serialized_json
   end
 end
