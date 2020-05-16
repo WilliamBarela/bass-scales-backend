fddbe54e2a5c2f4e6b0d6d60e915b505556814f8
William Barela <william.barela.dev@gmail.com>
2020-05-16 02:04:02 -0500

    add namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v0 do
+      resources :scales, only: [:show]
+      resources :feelings, only: [:create]
+    end
+  end
 end
