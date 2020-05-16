d231290de385a2050dad6814bf1efb58ea657328
William Barela <william.barela.dev@gmail.com>
2020-05-16 17:56:28 -0500

    add top feelings to serialized json

--- app/services/scale_serializer.rb
+++ app/services/scale_serializer.rb
@@ -6,7 +6,8 @@ class ScaleSerializer
   def to_serialized_json
     {
       :data => {
-        :scale => self.slim_scale
+        :scale => self.slim_scale,
+        :top_feelings => self.top_feelings
       }
     }
   end
@@ -15,5 +16,14 @@ class ScaleSerializer
     JSON.parse(
       @scale.to_json(:except => [:created_at, :updated_at])
     )
+  end
+
+  def top_feelings
+    Feeling.count_by_scale(@scale).collect do |f|
+      {
+        :adjective => f.adjective,
+        :count => f.count
+      }
+    end
   end
 end
