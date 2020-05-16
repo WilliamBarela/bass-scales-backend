43d1e431a3e66564a2bcb7f28245c1ad1b7c887d
William Barela <william.barela.dev@gmail.com>
2020-05-16 04:00:55 -0500

    move data serialization for scale show to app/services/ScaleSerializer.rb

--- app/services/scale_serializer.rb
+++ app/services/scale_serializer.rb
@@ -2,4 +2,23 @@ class ScaleSerializer
   def initialize(scale_object)
     @scale = scale_object
   end
+
+  def to_serialized_json
+    {
+      data: {
+       scale: {
+         id: @scale.id,
+         root: @scale.root,
+         mode: @scale.mode
+       },
+       feelings: @scale.feelings.collect do |f|
+         {
+           id: f.id,
+           adjective: f.adjective,
+           scale_id: f.scale_id
+         }
+       end
+     }
+    }
+  end
 end
