24e205a3b756f3ff33834fd20a3eac654b3dfc35
William Barela <william.barela.dev@gmail.com>
2020-05-16 18:16:04 -0500

    include sanitizer to ensure sql injection cannot happen, include limit capability

--- app/models/feeling.rb
+++ app/models/feeling.rb
@@ -1,8 +1,17 @@
 class Feeling < ApplicationRecord
   belongs_to :scale
 
-  scope :count_by_scale, ->(scale) { find_by_sql ["SELECT adjective, COUNT(*) AS count, :scale_id as SCALE_ID FROM feelings WHERE scale_id=:scale_id GROUP BY adjective ORDER BY count DESC;", {:scale_id => (scale.id.is_a? Integer) ? scale.id : 1}] }
- 
+  scope :count_by_scale, ->(scale, limit = 10) { 
+                           find_by_sql [
+                             "SELECT adjective, COUNT(*) AS count, :scale_id as SCALE_ID
+                              FROM feelings
+                              WHERE scale_id=:scale_id
+                              GROUP BY adjective
+                              ORDER BY count DESC
+                              LIMIT :limit;",
+                              Scale.sanitized_id_and_limit(scale, limit)
+                           ] }
+
   # READ: https://www.postgresql.org/docs/9.6/functions-matching.html#FUNCTIONS-POSIX-TABLE
   # scope :find_author, ->(search_params) { where("first_name ~* ?", "^#{search_params[:query]}")}
 end
--- app/models/scale.rb
+++ app/models/scale.rb
@@ -1,3 +1,9 @@
 class Scale < ApplicationRecord
   has_many :feelings
+
+  def self.sanitized_id_and_limit(scale, limit)
+    scale_id = (scale.id.is_a? Integer) ? scale.id : 1
+    limit = (limit.is_a? Integer) ? limit : 10
+    {:scale_id => scale_id, :limit => limit }
+  end
 end
