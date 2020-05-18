8569c45f22f4acd9aa98feefd3e9b07496357a8b
William Barela <william.barela.dev@gmail.com>
2020-05-18 00:02:58 -0500

    add working feeling#create method

--- app/controllers/api/v0/feelings_controller.rb
+++ app/controllers/api/v0/feelings_controller.rb
@@ -1,7 +1,14 @@
 class Api::V0::FeelingsController < ApplicationController
+
   def create
-    scale = Scale.find_by(feeling_params[:scale_id])
-    feeling = scale.feelings.build(feeling_params[:feeling_adjective])
+    scale = Scale.find_by_id(feeling_params[:scale_id])
+    feeling = scale.feelings.build(:adjective => feeling_params[:feeling_adjective])
+
+    if feeling.save
+      render json: {:message => "Sucess!!"}
+    else
+      render json: {:message => "Failure!!"}
+    end
   end
 
   private
