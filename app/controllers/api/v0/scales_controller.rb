59311a53100ece9e75bcb1663420282bd0ceb0e0
William Barela <william.barela.dev@gmail.com>
2020-05-27 00:13:37 -0500

    add additional validations to feelings

--- app/controllers/api/v0/scales_controller.rb
+++ app/controllers/api/v0/scales_controller.rb
@@ -1,7 +1,7 @@
 class Api::V0::ScalesController < ApplicationController
   def show
     scale = Scale.find_by(id: params[:id])
-    feelings_limit = 5
+    feelings_limit = 10
     render json: ScaleSerializer.new(scale, feelings_limit).scale_feelings_json
   end
 end
