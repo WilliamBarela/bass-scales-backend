8a4f72b5c66ebf37e6f15da1d6820d51506237af
William Barela <william.barela.dev@gmail.com>
2020-05-27 00:13:11 -0500

    change feeling limit

--- app/models/feeling.rb
+++ app/models/feeling.rb
@@ -4,6 +4,10 @@ class Feeling < ApplicationRecord
   validates :adjective,
     presence: true
 
+  validate :valid_adjective_length?
+  validate :adjective_single_word?
+  validate :adjective_alphabetic_only?
+
   scope :count_by_scale, ->(scale, limit = 10) { 
                            find_by_sql [
                              "SELECT adjective, COUNT(*) AS count, :scale_id as SCALE_ID
@@ -14,6 +18,18 @@ class Feeling < ApplicationRecord
                               LIMIT :limit;",
                               Scale.sanitized_id_and_limit(scale, limit)
                            ] }
+
+  def valid_adjective_length?
+    errors.add(:adjective, "must be between 3 and 20 characters.") unless self.adjective.length.between?(3,20)
+  end
+
+  def adjective_single_word?
+    errors.add(:adjective, "must be a single word.") if self.adjective.include?(" ")
+  end
+
+  def adjective_alphabetic_only?
+    errors.add(:adjective, "must contain only alphabetic characters.") unless self.adjective.match?(/\A[a-zA-Z]+\.?\z/)
+  end
 
   # READ: https://www.postgresql.org/docs/9.6/functions-matching.html#FUNCTIONS-POSIX-TABLE
   # scope :find_author, ->(search_params) { where("first_name ~* ?", "^#{search_params[:query]}")}
